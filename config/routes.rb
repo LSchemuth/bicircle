61d0fd9bba1453f7ee94af8c9dbf7fec88a2abc3
Luissa Schemuth <luissa.schemuth@outlook.com>
2020-08-05 12:25:07 +0200

    implemented my bikes action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get '/bikes/my_bikes', to: 'bikes#my_bikes'
   resources :bikes do
     resources :reservations, only: [ :index, :new, :create ]
   end
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
       get :my_reservations
     end
   end
+
 end
